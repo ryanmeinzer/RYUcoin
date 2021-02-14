c1f2a3210ef66daf2511ca472abe426945f80cb7
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2021-02-13 17:00:53 -0800

    included chain validation

--- block.js
+++ block.js
@@ -29,13 +29,3 @@ class Block {
 }
 
 module.exports = Block
-
-// const block1 = new Block({
-//     timestamp: '01/01/01',
-//     lastHash: 'foo-lastHash',
-//     hash: 'foo-hash',
-//     data: 'foo-data'
-// })
-
-// console.log('block1', block1)
-
