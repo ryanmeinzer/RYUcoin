da3d74b13e67ad2983b217faab5c330d840531c1
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2021-02-14 20:34:22 -0800

    included transaction updates in the API

--- index.js
+++ index.js
@@ -34,10 +34,15 @@ app.post('/api/mine', (req, res) => {
 app.post('/api/transact', (req, res) => {
     const { amount, recipient } = req.body
 
-    let transaction
+    let transaction = transactionPool
+        .existingTransaction({ inputAddress: wallet.publicKey })
 
     try {
-        transaction = wallet.createTransaction({ recipient, amount })
+        if (transaction) {
+            transaction.update({ senderWallet: wallet, recipient, amount })
+        } else { 
+            transaction = wallet.createTransaction({ recipient, amount })
+        } 
     } catch (error) {
         return res.status(400).json({ type: 'error', message: error.message })
     }
@@ -46,7 +51,7 @@ app.post('/api/transact', (req, res) => {
 
     console.log('transactionPool', transactionPool)
 
-    res.json({ transaction })
+    res.json({ type: 'success', transaction })
 })
 
 const syncChains = () => {
