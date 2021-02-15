65f9729d23b55cd2346e668b4c1a1fff5f65ff94
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2021-02-14 21:21:39 -0800

    grabbed valid transactions only

--- wallet/transaction-pool.js
+++ wallet/transaction-pool.js
@@ -1,3 +1,6 @@
+const Transaction = require('./transaction')
+const Transactions = require('./transaction')
+
 class TransactionPool {
     constructor() {
         this.transactionMap = {}
@@ -15,6 +18,12 @@ class TransactionPool {
         const transactions = Object.values(this.transactionMap)
 
         return transactions.find(transaction => transaction.input.address === inputAddress)
+    }
+
+    validTransactions() {
+        return Object.values(this.transactionMap).filter(
+            transaction => Transaction.validTransaction(transaction)
+        )
     }
 }
 
