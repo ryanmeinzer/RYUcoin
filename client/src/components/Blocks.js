97154466ee035257effb3573522143402aea6f5e
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2021-02-15 22:19:52 -0800

    added block component

--- client/src/components/Blocks.js
+++ client/src/components/Blocks.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import Block from '../../../blockchain/block'
+import Block from './Block'
 
 class Blocks extends Component {
     state = { blocks: [] }
@@ -19,7 +19,7 @@ class Blocks extends Component {
                 {
                     this.state.blocks.map(block => {
                         return (
-                            <div key={block.hash} className="Block">{block.hash}</div>
+                            <Block key={block.hash} block={block} />
                         )
                     })
                 }
