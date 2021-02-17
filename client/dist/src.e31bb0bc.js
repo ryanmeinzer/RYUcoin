11cb6142d1c6d6136511ee18f16ce45a18c19e69
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2021-02-17 00:49:52 -0800

    integrated known addresses on backend

--- client/dist/src.e31bb0bc.js
+++ client/dist/src.e31bb0bc.js
@@ -47396,7 +47396,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64197" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "63097" + '/');
 
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
