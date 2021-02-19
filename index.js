6c357c95edce8091d35999208bfec90ea7ed951b
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2021-02-19 10:50:14 -0800

    updated config vars and redis url to TLS

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const TransactionMiner = require('./app/transaction-miner')
 const isDevelopment = process.env.ENV === 'development' 
 
 const REDIS_URL = isDevelopment ? 'redis://127.0.0.1:6379' :
-    'redis://:pec638600f47ca76f4c8f3d9763b2d71b3a168e3d69ae89f610dafe036335b771@ec2-54-144-175-109.compute-1.amazonaws.com:6759' 
+    'redis://:pec638600f47ca76f4c8f3d9763b2d71b3a168e3d69ae89f610dafe036335b771@ec2-52-22-235-152.compute-1.amazonaws.com:13020' 
 const DEFAULT_PORT = 3000
 const ROOT_NODE_ADDRESS = `http://localhost:${DEFAULT_PORT}`
 
