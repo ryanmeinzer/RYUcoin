398c70057e70de21c99a421846fc5440774dc345
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2021-02-19 09:58:55 -0800

    updated redis URI

--- client/dist/src.e31bb0bc.js
+++ client/dist/src.e31bb0bc.js
@@ -47452,7 +47452,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "56552" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59418" + '/');
 
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
